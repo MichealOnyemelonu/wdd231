b2748bf67717a85e521f3ccdeb85984f1812ef01
MichealOnyemelonu <monyemelonu@gmail.com>
2026-06-10 00:41:02 +0200

    added lazy loading feature to the image in the .js file and logic

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -24,7 +24,7 @@ places.forEach(place => {
     card.innerHTML = `
         <h2>${place.name}</h2>
         <figure>
-            <img src="${place.image}" alt="${place.name}">
+            <img src="${place.image}" alt="${place.name}" loading="lazy">
         </figure>
         <address>${place.address}</address>
         <p>${place.description}</p>
